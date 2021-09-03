15fbf03e13a05b9501f5dbde3f7c52e23f34791c
omm_sss <journy002@naver.com>
2021-09-03 10:20:08 +0900

    update

--- src/component/Number.jsx
+++ src/component/Number.jsx
@@ -3,9 +3,12 @@ import ReactPlayer from 'react-player';
 
 function Number() {
     const [playVideo, setPlayVideo] = useState([{
+        id: 1,
         url: 'https://vt.tumblr.com/tumblr_o600t8hzf51qcbnq0_480.mp4',
         videoState: false
     }]);
+
+    const [videoIndex, setVideoIndex] = useState(0);
 
     const onPlay = () => {
         setPlayVideo({
@@ -21,15 +24,23 @@ function Number() {
         })
     }
 
+    const autoPlay = () => {
+        setVideoIndex(
+            ...videoIndex,
+            playVideo[videoIndex + 1]
+        )
+    }
+    
 
     return (
         <>
             <ReactPlayer
-                url={playVideo[0].url}
+                url={playVideo[videoIndex].url}
                 playing={playVideo.videoState}
             />
             <button onClick={onPlay} >Play</button>
             <button onClick={pausePlay} >Pause</button>
+            <button onClick={autoPlay} >Next</button>
         </>
     )
 }
